b81df4e1e50fa5a7fa8ad7578cfa32dc56f9beed
han19githtbr <milliance23@gmail.com>
2024-02-05 10:00:07 -0300

    update...

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,2 +1,2 @@
-export const FACELIST_API = 'http://localhost:3000'
-//export const FACELIST_API = 'https://franceshanfojack.netlify.app'
+//export const FACELIST_API = 'http://localhost:3000'
+export const FACELIST_API = 'https://plataformafrances.web.app'
